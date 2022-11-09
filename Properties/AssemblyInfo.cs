19f2355313a7ed6db80b5ffd0dbfc407698e83e0
XoX-Toxic <77903650+ToxicStuff@users.noreply.github.com>
2022-11-09 16:12:28 +0100

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using MelonLoader;
+using TabExtension;
+
+[assembly: MelonInfo(typeof(TabExtensionMod), "TabExtension", "1.4.0", "DragonPlayer (Ported XoX-Toxic)", "https://github.com/ToxicStuff/TabExtension")]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: AssemblyCompany("TabExtension")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("TabExtension")]
+[assembly: AssemblyTitle("TabExtension")]
+[assembly: AssemblyVersion("1.0.0.0")]
